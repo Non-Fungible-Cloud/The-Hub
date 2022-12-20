0c1d507dd35fd7f2a13833eb0e0af226d0d1ecf5
ferrabled <frabascol@gmail.com>
2022-12-21 00:34:55 +0100

    Marketplace Item view

--- web/front/the-hub/src/components/card/itemDetailCard.tsx
+++ web/front/the-hub/src/components/card/itemDetailCard.tsx
@@ -9,23 +9,29 @@ const ItemDetailCard = (props: {
     price?: number;
 }) => {
     return(
-        <Card className="w-[600px] h-76 p-9 border-2 border-black shadow" sx={{ boxShadow: '5px 7px 10px rgba(0,0,0,0.6)', borderRadius:'15px', backgroundColor: "rgba(255,255,255,0.6);"}}>
+        <Card className="w-[1400px] h-[650px] mt-24 p-9 border-2 border-black shadow" sx={{ boxShadow: '5px 7px 10px rgba(0,0,0,0.6)', borderRadius:'15px', backgroundColor: "rgba(255,255,255,0.6);"}}>
             <div className="flex flex-row justify-between gap-5">
-                <img src="/THH_black.png" alt="logo" className="h-36 sm:h-28 min-[320px]:h-24" />
-                <div className="flex flex-col justify-between">
+                <img src={props.image} alt="item" className=""/>
+                <div className="flex flex-col justify-evenly content-between mr-28">
                     <Typography variant="h3">
-                    WELCOME 
+                        {props.name} 
                     </Typography>
-                    <Typography variant="h3">
-                        TO
+                    <Typography variant="h6" className="max-w-md">
+                        {props.description}
                     </Typography>
+                    <div className="flex flex-row justify-evenly items-center mt-6">
+                        <Button className="h-10 w-[180px]" style={{
+                            backgroundImage: 'linear-gradient(45deg, #e74c3c, #8e44ad, #f1c40f)', color:'black', 
+                            boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)', cursor: 'pointer', 
+                            transition: '0.4s', backgroundSize: '200%',}}> 
+                        BUY! </Button>     
+                        <Typography> {props.price} ETH </Typography>    
+                    </div>
+                    
                 </div>
                 
             </div>
-                    <Button className="h-10 w-[280px]" style={{
-                        backgroundImage: 'linear-gradient(45deg, #e74c3c, #8e44ad, #f1c40f)', color:'black', 
-                        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)', cursor: 'pointer', 
-                        transition: '0.4s', backgroundSize: '200%', marginTop:'20px'}}> BUY! </Button>
+                    
                 </Card>
     );
 };
--- web/front/the-hub/src/pages/marketplaceItem.tsx
+++ web/front/the-hub/src/pages/marketplaceItem.tsx
@@ -1,19 +1,36 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Card, Typography } from "@mui/material";
 
 import Page from "./page";
 import ItemDetailCard from "src/components/card/itemDetailCard";
+import { useParams } from "react-router-dom";
 
 
 const MarketplaceItemPage = () => {
 
     //TODO: CALL BLOCKCHAIN TO 
     //GET DETAILS FROM THE ASSET AND SEND IT INTO THE DETAIL CARD
+    const [item, setItem] = useState<any>();
+    const [isLoading, setIsLoading] = useState(true);
+
+
+    const idNumber = Number(useParams().id);
+
+    useEffect(() => {
+        //Blockchain Connection
+        setItem({name: "Test", id: 1, image: "/clothing/trousers.png", description: "With this trousers, you can show off your style in-game", price: 1});
+        setIsLoading(false);
+    }, []);
+
+
 
     return(
+
             <Page background="url(/hereticsback.png)">
                 <div className="h-full w-full flex justify-center items-center">
-                         <ItemDetailCard></ItemDetailCard>           
+                         {!isLoading && (
+                            <ItemDetailCard image={item.image} name={item.name} description={item.description} price={item.price}></ItemDetailCard>           
+                         )}
                 </div>
                 
 
