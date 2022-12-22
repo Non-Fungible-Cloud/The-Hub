e03818ee626d17e7971d98afbe6c753f43365827
nicodeory-playplay <nicolas.deory@playplay.com>
2022-12-22 13:24:03 +0100

    added effect to image

--- web/front/the-hub/src/components/card/itemDetailCard.tsx
+++ web/front/the-hub/src/components/card/itemDetailCard.tsx
@@ -1,40 +1,44 @@
-import React from "react";
-import { Button, Card, Typography } from "@mui/material";
+import { Card, Typography } from "@mui/material";
+import ActionButton from "src/components/buttons/actionbutton";
 
 const ItemDetailCard = (props: {
-    name?: string;
-    id?: number;
-    image?: string;
-    description?: string;
-    price?: number;
+  name?: string;
+  id?: number;
+  image?: string;
+  description?: string;
+  price?: number;
 }) => {
-    return(
-        <Card className="w-[1400px] h-[650px] mt-24 p-9 border-2 border-black shadow" sx={{ boxShadow: '5px 7px 10px rgba(0,0,0,0.6)', borderRadius:'15px', backgroundColor: "rgba(255,255,255,0.6);"}}>
-            <div className="flex flex-row justify-between gap-5">
-                <img src={props.image} alt="item" className=""/>
-                <div className="flex flex-col justify-evenly content-between mr-28">
-                    <Typography variant="h3">
-                        {props.name} 
-                    </Typography>
-                    <Typography variant="h6" className="max-w-md">
-                        {props.description}
-                    </Typography>
-                    <div className="flex flex-row justify-evenly items-center mt-6">
-                        <Button className="h-10 w-[180px]" style={{
-                            backgroundImage: 'linear-gradient(45deg, #e74c3c, #8e44ad, #f1c40f)', color:'black', 
-                            boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)', cursor: 'pointer', 
-                            transition: '0.4s', backgroundSize: '200%',}}> 
-                        BUY! </Button>     
-                        <Typography> {props.price} ETH </Typography>    
-                    </div>
-                    
-                </div>
-                
-            </div>
-                    
-                </Card>
-    );
+  return (
+    <Card
+      className="w-[1400px] h-[650px] mt-24 p-9 border-2 border-black shadow"
+      sx={{
+        border: "solid 1px #222",
+        boxShadow: "5px 7px 10px rgba(0,0,0,0.2)",
+        backdropFilter: "blur(5px)",
+        borderRadius: "10px",
+        backgroundColor: "rgba(255,255,255,0.6);",
+      }}
+    >
+      <div className="flex flex-row justify-between gap-5">
+        <img
+          src={props.image}
+          alt="item"
+          className=""
+          style={{ filter: "drop-shadow(-5px -5px 0px #8e44ad) drop-shadow(5px 5px 0px #f23927) drop-shadow(10px 10px 5px #8e44ad44)" }}
+        />
+        <div className="flex flex-col justify-evenly content-between mr-28">
+          <Typography variant="h3">{props.name}</Typography>
+          <Typography variant="h6" className="max-w-md">
+            {props.description}
+          </Typography>
+          <div className="flex flex-row justify-evenly items-center mt-6">
+            <ActionButton>Buy now</ActionButton>
+            <Typography> {props.price} ETH </Typography>
+          </div>
+        </div>
+      </div>
+    </Card>
+  );
 };
-
 
 export default ItemDetailCard;
--- web/front/the-hub/src/pages/marketplaceItem.tsx
+++ web/front/the-hub/src/pages/marketplaceItem.tsx
@@ -1,43 +1,43 @@
-import React, { useEffect, useState } from "react";
-import { Button, Card, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
 
-import Page from "./page";
-import ItemDetailCard from "src/components/card/itemDetailCard";
 import { useParams } from "react-router-dom";
-
+import ItemDetailCard from "src/components/card/itemDetailCard";
+import Page from "./page";
 
 const MarketplaceItemPage = () => {
+  //TODO: CALL BLOCKCHAIN TO
+  //GET DETAILS FROM THE ASSET AND SEND IT INTO THE DETAIL CARD
+  const [item, setItem] = useState<any>();
+  const [isLoading, setIsLoading] = useState(true);
 
-    //TODO: CALL BLOCKCHAIN TO 
-    //GET DETAILS FROM THE ASSET AND SEND IT INTO THE DETAIL CARD
-    const [item, setItem] = useState<any>();
-    const [isLoading, setIsLoading] = useState(true);
+  const idNumber = Number(useParams().id);
 
+  useEffect(() => {
+    //Blockchain Connection
+    setItem({
+      name: "Test",
+      id: 1,
+      image: "/clothing/trousers.png",
+      description: "With this trousers, you can show off your style in-game",
+      price: 1,
+    });
+    setIsLoading(false);
+  }, []);
 
-    const idNumber = Number(useParams().id);
-
-    useEffect(() => {
-        //Blockchain Connection
-        setItem({name: "Test", id: 1, image: "/clothing/trousers.png", description: "With this trousers, you can show off your style in-game", price: 1});
-        setIsLoading(false);
-    }, []);
-
-
-
-    return(
-
-            <Page background="url(/hereticsback.png)">
-                <div className="h-full w-full flex justify-center items-center">
-                         {!isLoading && (
-                            <ItemDetailCard image={item.image} name={item.name} description={item.description} price={item.price}></ItemDetailCard>           
-                         )}
-                </div>
-                
-
-            </Page>
-    );
-
+  return (
+    <Page background="url(/hereticsback.png)">
+      <div className="h-full w-full flex justify-center items-center">
+        {!isLoading && (
+          <ItemDetailCard
+            image={item.image}
+            name={item.name}
+            description={item.description}
+            price={item.price}
+          ></ItemDetailCard>
+        )}
+      </div>
+    </Page>
+  );
 };
-
 
 export default MarketplaceItemPage;
